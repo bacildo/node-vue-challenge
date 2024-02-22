c9e4c1f1accc1ab8e3d5403bcef867a25b623c55
bacildo <dbacildodasilva@gmail.com>
2024-02-21 23:11:49 -0300

    created modal for deleting, and change some function names to english

--- edtech-student-management/src/components/StudentsList.vue
+++ edtech-student-management/src/components/StudentsList.vue
@@ -20,29 +20,71 @@
             >
           </v-list-item-content>
           <v-list-item-action>
-            <v-btn @click="updateStudent(student)">Editar</v-btn>
-            <v-btn @click="deleteStudent(student.id_student)">Excluir</v-btn>
+            <v-btn @click="updateStudentModal(student)">Editar</v-btn>
+            <v-btn @click="deleteStudentModal(student)">Excluir</v-btn>
           </v-list-item-action>
         </v-list-item>
       </v-list>
     </v-card-text>
-    <v-alert v-if="showAlertOnDelete" type="success" dismissible>
-      Aluno excluído com sucesso!
-    </v-alert>
 
-    <v-alert v-if="showAlertOnSave" type="success" dismissible>
-      Aluno salvo com sucesso!
-    </v-alert>
+    <!-- Dialog de sucesso ao excluir -->
+    <v-dialog v-model="showSuccessDialog" max-width="500px">
+      <v-card>
+        <v-card-title>Sucesso!</v-card-title>
+        <v-card-text> Aluno excluído com sucesso! </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" @click="showSuccessDialog = false">OK</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
-    <!-- <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn @click="showStudentList = false">Voltar</v-btn>
-    </v-card-actions> -->
+    <!-- Dialog de sucesso ao editar -->
+    <v-dialog v-model="showEditSuccessDialog" max-width="500px">
+      <v-card>
+        <v-card-title>Sucesso!</v-card-title>
+        <v-card-text> Aluno editado com sucesso! </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" @click="showEditSuccessDialog = false"
+            >OK</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <!-- Dialog de sucesso ao salvar -->
+    <v-dialog v-model="showSaveSuccessDialog" max-width="500px">
+      <v-card>
+        <v-card-title>Sucesso!</v-card-title>
+        <v-card-text> Aluno salvo com sucesso! </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" @click="showSaveSuccessDialog = false"
+            >OK</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <!-- Modal de confirmação de exclusão -->
+    <v-dialog v-model="showDeleteModal" max-width="500px">
+      <v-card>
+        <v-card-title>Confirmar Exclusão</v-card-title>
+        <v-card-text> Tem certeza que deseja excluir este aluno? </v-card-text>
+        <v-card-actions>
+          <v-btn color="red darken-1" @click="confirmDeleteStudent"
+            >Confirmar</v-btn
+          >
+          <v-btn color="blue darken-1" @click="cancelDeleteStudent"
+            >Cancelar</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
     <v-card-actions>
       <v-btn @click="toggleListaAlunos">{{
         showStudentList ? "Ocultar Alunos" : "Mostrar Alunos"
       }}</v-btn>
-      <v-btn @click="createStudent(student)">Criar Aluno</v-btn>
+      <v-btn @click="createStudentModal(student)">Criar Aluno</v-btn>
     </v-card-actions>
 
     <!-- Modal de edição -->
@@ -65,8 +107,10 @@
           ></v-text-field>
         </v-card-text>
         <v-card-actions>
-          <v-btn color="blue darken-1" @click="saveUpdateStudent">Salvar</v-btn>
-          <v-btn color="red darken-1" @click="cancelUpdate">Cancelar</v-btn>
+          <v-btn color="blue darken-1" @click="updateStudent">Salvar</v-btn>
+          <v-btn color="red darken-1" @click="cancelUpdateStudent"
+            >Cancelar</v-btn
+          >
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -89,12 +133,24 @@
             label="E-mail"
             v-model="createdStudent.email"
           ></v-text-field>
+          <span
+            v-if="
+              !createdStudent.name ||
+              !createdStudent.register ||
+              !createdStudent.cpf ||
+              !createdStudent.email
+            "
+            class="error-message"
+            >Todos os campos são obrigatórios</span
+          >
         </v-card-text>
         <v-card-actions>
           <v-btn color="blue darken-1" @click="saveStudent(createdStudent)"
             >Salvar</v-btn
           >
-          <v-btn color="red darken-1" @click="cancelSaveStudent">Cancelar</v-btn>
+          <v-btn color="red darken-1" @click="cancelSaveStudent"
+            >Cancelar</v-btn
+          >
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -109,11 +165,14 @@ export default {
     return {
       students: [],
       showStudentList: false,
-      showAlertOnDelete: false,
-      showAlertOnSave: false,
+      showSuccessDialog: false,
+      showEditSuccessDialog: false,
+      showSaveSuccessDialog: false,
       editedStudent: {}, // Armazena as informações do aluno em edição
       showEditModal: false, // Controla a modal de editing
       showCreateModal: false,
+      showDeleteModal: false, // Controla a modal de exclusão
+      studentToDelete: null, // Armazena o aluno a ser excluído
       createdStudent: {
         name: "",
         register: "",
@@ -123,7 +182,7 @@ export default {
     };
   },
   methods: {
-    async carregarAlunos() {
+    async loadStudents() {
       try {
         const response = await httpRequest.get("/students");
         this.students = response.data;
@@ -132,72 +191,104 @@ export default {
       }
     },
     async saveStudent(student) {
+      if (
+        !student.name ||
+        !student.register ||
+        !student.cpf ||
+        !student.email
+      ) {
+        console.error("Todos os campos são obrigatórios");
+        return;
+      }
+
       try {
         const response = await httpRequest.post("/students", student);
         console.log("Student created:", response.data);
-        this.showAlertOnSave = true;
-        this.carregarAlunos();
+        this.showSaveSuccessDialog = true;
+        (this.showCreateModal = false), this.loadStudents();
 
         setTimeout(() => {
-          this.showAlertOnSave = false;
+          this.showSaveSuccessDialog = false;
         }, 3000); // 3 segundos        this.showCreateModal = false;
       } catch (error) {
         console.error("Error to save the student:", error);
       }
     },
 
-    updateStudent(student) {
-      this.editedStudent = { ...student }; // Clona os dados do aluno para a variável editedStudent
-      this.showEditModal = true; // Abre o modal de edição
-    },
-
-    createStudent(student) {
-     
-      this.createdStudent = { ...student }; // Clona os dados do aluno para a variável createdStudent
-      this.showCreateModal = true; // Abre o modal de criação
-    },
-
-    async saveUpdateStudent() {
+    async updateStudent() {
       try {
         const response = await httpRequest.put(
           `/students/${this.editedStudent.id_student}`,
           this.editedStudent
         );
         console.log("Student updated:", response.data);
-        this.carregarAlunos(); // Atualize a lista de alunos após salvar as alterações
-        this.showEditModal = false; // Feche o modal
+        this.showEditSuccessDialog = true;
+        (this.showEditModal = false), this.loadStudents();
+
+        setTimeout(() => {
+          this.showEditSuccessDialog = false;
+        }, 3000); // 3 segundos
       } catch (error) {
         console.error("Error to update the student:", error);
       }
-    },
-    cancelUpdate() {
-      this.showEditModal = false; // Fecha o modal sem salvar
-    },
-
-    cancelSaveStudent() {
-      this.showCreateModal = false; // Fecha o modal sem salvar
     },
 
     async deleteStudent(id) {
       try {
         await httpRequest.delete(`/students/${id}`);
         console.log("Student deleted");
-        this.showAlertOnDelete = true;
-        this.carregarAlunos();
+        this.showSuccessDialog = true;
+        this.loadStudents();
 
         setTimeout(() => {
-          this.showAlertOnDelete = false;
+          this.showSuccessDialog = false;
         }, 3000); // 3 segundos
       } catch (error) {
         console.error("Error to delete the student:", error);
       }
+      this.showDeleteModal = false;
     },
+
+    confirmDeleteStudent() {
+      if (this.studentToDelete) {
+        // Se houver um aluno para excluir
+        this.deleteStudent(this.studentToDelete.id_student); // Exclui o aluno
+        this.showDeleteModal = false; // Fecha a modal de exclusão
+      }
+    },
+
+    updateStudentModal(student) {
+      this.editedStudent = { ...student }; // Clona os dados do aluno para a variável editedStudent
+      this.showEditModal = true; // Abre o modal de edição
+    },
+
+    createStudentModal(student) {
+      this.createdStudent = { ...student }; // Clona os dados do aluno para a variável createdStudent
+      this.showCreateModal = true; // Abre o modal de criação
+    },
+
+    deleteStudentModal(student) {
+      this.studentToDelete = student; // Armazena o aluno a ser excluído
+      this.showDeleteModal = true; // Abre a modal de exclusão
+    },
+
+    cancelUpdateStudent() {
+      this.showEditModal = false; // Fecha o modal sem salvar
+    },
+
+    cancelSaveStudent() {
+      this.showCreateModal = false; // Fecha o modal sem salvar
+    },
+    cancelDeleteStudent() {
+      this.showDeleteModal = false;
+    },
+
     toggleListaAlunos() {
       this.showStudentList = !this.showStudentList;
     },
   },
   created() {
-    this.carregarAlunos();
+    this.loadStudents();
   },
 };
 </script>
